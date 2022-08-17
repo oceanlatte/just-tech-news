d6fd816d0b1cf4a84ad9567414ac8e6a619a47b1
ocean <osciris.veliz@gmail.com>
2022-08-17 16:59:24 -0500

    removed commented out line that was not being used

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -63,8 +63,6 @@ router.post('/login', (req, res) => {
       res.status(400).json({ message: 'No user with that email address found.'});
       return;
     }
-    // res.json({ user: dbUserData });
-
     // verify user
     const validPassword = dbUserData.checkPassword(req.body.password);
 
