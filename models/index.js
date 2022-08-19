91c6d9cc51ddb720b18467b70caac543a4f03ba2
ocean <osciris.veliz@gmail.com>
2022-08-18 19:03:12 -0500

    create API routes for the Post Model

--- models/index.js
+++ models/index.js
@@ -3,4 +3,14 @@ const Post = require('./Post');
 
 // collecting and exporting model data
 
+
+//create associations
+User.hasMany(Post, { 
+  foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,12 @@
 // collect all of the API routes and package them together
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
 
 
 // take routes from user-routes and prefix them with path /users in this file
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 
 module.exports = router;
