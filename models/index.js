7ffa2c1b2c0ead991adff3d26ea93f20fb68343c
ocean <osciris.veliz@gmail.com>
2022-08-19 20:51:47 -0500

    created the comment model

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require('./User');
 const Post = require('./Post');
 const Vote = require('./Vote');
+const Comments = require('./Comments');
 
 // collecting and exporting model data
 
@@ -51,4 +52,20 @@ Post.hasMany(Vote, {
   foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote };
+// -----Comments Associations-------
+Comments.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+Comments.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comments, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comments, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comments };
