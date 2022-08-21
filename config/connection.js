36b54724e47e40b3b58551677b389f37937c5d45
ocean <osciris.veliz@gmail.com>
2022-08-20 20:52:21 -0500

    populate the template with sequelize data

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,8 @@ const Sequelize = require('sequelize'); // import Sequelize constructor from lib
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: 'localhost',
   dialect: 'mysql',
-  port: 3306
+  port: 3306,
+  logging: false
 });
 
 module.exports = sequelize;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,42 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comments } = require('../models');
 
 // file contains all the user-facing routes, like login and homepage
 
 
 router.get('/', (req, res) => {
-  res.render('homepage');
+  Post.findAll({
+    attributes: [
+      'id', 
+      'post_url', 
+      'title', 
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: Comments,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+  .then(dbPostData => {
+    const posts = dbPostData.map(post => post.get({ plain: true }));
+    res.render('homepage', { posts });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
